e9bb1cdfc195ceca9b018e2b44f2fb9ab8662bc9
Jesus Perez <jesuspereza3108@gmail.com>
2026-02-20 18:46:55 -0400

    Lista enlazada

--- src/edu/unimet/structures/ListaProcesos.java
+++ src/edu/unimet/structures/ListaProcesos.java
@@ -8,6 +8,33 @@ package edu.unimet.structures;
  *
  * @author jesus alejandro
  */
+public class Nodo {
+    PCB proceso;
+    Nodo siguiente;
+
+    public Nodo(PCB proceso) {
+        this.proceso = proceso;
+        this.siguiente = null;
+    }
+}
+
 public class ListaProcesos {
-    
+    private Nodo cabeza;
+
+    public void agregar(PCB p) {
+        Nodo nuevo = new Nodo(p);
+        if (cabeza == null) {
+            cabeza = nuevo;
+        } else {
+            Nodo temp = cabeza;
+            while (temp.siguiente != null) {
+                temp = temp.siguiente;
+            }
+            temp.siguiente = nuevo;
+        }
+    }
+
+    public Nodo getCabeza() {
+        return cabeza;
+    }
 }
