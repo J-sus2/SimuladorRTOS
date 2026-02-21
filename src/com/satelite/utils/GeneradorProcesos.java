edfd8758a067ea7a835713d1bc94e2c1afcf97f5
Jesus Perez <jesuspereza3108@gmail.com>
2026-02-20 20:00:56 -0400

    Logica para generar 20 procesos de manera aleatoria

--- src/com/satelite/utils/GeneradorProcesos.java
+++ src/com/satelite/utils/GeneradorProcesos.java
@@ -8,6 +8,26 @@ package com.satelite.utils;
  *
  * @author jesus alejandro
  */
+import com.satelite.model.PCB;
+import com.satelite.structures.ListaProcesos;
+import java.util.Random;
+
 public class GeneradorProcesos {
-    
+    private static int contadorID = 1;
+    private static Random random = new Random();
+
+    public static void generar20Aleatorios(ListaProcesos lista) {
+        for (int i = 0; i < 20; i++) {
+            // Datos aleatorios para simular el microsatélite
+            int instrucciones = 10 + random.nextInt(50); // de 10 a 60 instrucciones
+            int prioridad = 1 + random.nextInt(5);      // prioridad 1 (alta) a 5 (baja)
+            int deadline = 100 + random.nextInt(200);   // deadline entre 100 y 300 ciclos
+            
+            PCB nuevo = new PCB(contadorID, "SatTask-" + contadorID, prioridad, deadline);
+            nuevo.totalInstrucciones = instrucciones; // Asumiendo que añadiste este campo al PCB
+            
+            lista.agregar(nuevo);
+            contadorID++;
+        }
+    }
 }
