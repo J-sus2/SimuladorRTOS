9e141a403ee1aa60a2845077c1f08b17b6d0b8ac
Jesus Perez <jesuspereza3108@gmail.com>
2026-02-21 01:35:57 -0400

    Creation of Public Enum EstadoProceso in EstadoProceso class

--- src/com/satelite/model/EstadoProceso.java
+++ src/com/satelite/model/EstadoProceso.java
@@ -8,6 +8,13 @@ package com.satelite.model;
  *
  * @author jesus alejandro
  */
-public class EstadoProceso {
-    
+
+public enum EstadoProceso {
+    NUEVO,
+    LISTO,
+    EJECUCION,
+    BLOQUEADO,
+    TERMINADO,
+    LISTO_SUSPENDIDO,
+    BLOQUEADO_SUSPENDIDO
 }
